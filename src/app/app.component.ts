3e9e70c119446fb59efa0fd5fb2b15e5ab0501dd
leomanne <leomanne2000@gmail.com>
2023-01-24 14:41:51 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = 'Prenotazione biglietti';
+  
+  reservation: boolean = false; //used to enable form 
+
+
+  startReservation() {
+    // funzione che attiva il form per la prenotazione
+    this.reservation = true;
+  }
 }

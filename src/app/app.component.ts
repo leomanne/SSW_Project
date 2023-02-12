889fe9aafc744359555acead95f7fd631cca371a
leomanne <leomanne2000@gmail.com>
2023-02-12 16:58:32 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   
   title = 'Prenotazione biglietti';
   reservation: boolean = false; //usato per abilitare il form 
-  listaEventi: string[] = ['422d9016'];
+  listaEventi: string[] = undefined;
   platea: any[] = []; //array generico che contiene i posti in platea
   stage: any[] = [];  //uguale
   fastReservation: boolean = false; //variabile booleana per capire se e' stata richiesta una reservation rapida
